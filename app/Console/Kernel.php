b68d6e1144b108f75288b93862483ab7647602e0
SOSTheBlack <jeancesargarcia@gmail.com>
2023-10-19 03:25:38 -0300

    chore: criado cron p rodar a cada 10 min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->job(new SearchAndSaveItemsJob())->everyTenMinutes();
         $schedule->job(new SearchAndSaveItemsJob())->everyMinute();
     }
 
